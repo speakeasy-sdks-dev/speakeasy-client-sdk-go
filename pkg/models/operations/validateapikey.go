b1c767eef9ac6b6d542410b6ed1ac7727ae08b99
Speakeasy CI <ci@speakeasyapi.dev>
2023-01-12 00:13:53 +0000

    ci: regenerated with OpenAPI Doc 0.2.0, Speakeay CLI 0.18.3

---
+++ pkg/models/operations/validateapikey.go
@@ -0,0 +1,11 @@
+package operations
+
+import (
+	"github.com/speakeasy-api/speakeasy-client-sdk-go/pkg/models/shared"
+)
+
+type ValidateAPIKeyResponse struct {
+	ContentType string
+	Error       *shared.Error
+	StatusCode  int64
+}
